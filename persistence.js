9120465dd6070d11cbdf0cefb862b01a25bc9ba2
yankovskyandrey <YankovskyAndrey@gmail.com>
2020-08-22 18:46:17 +0300

    move server and client to separate folders

--- persistence.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs'
-
-export const save = (data) => {
-  fs.writeFileSync('./lobbies.json', JSON.stringify(data))
-}
-
-export const load = () => {
-  return JSON.parse(fs.readFileSync('./lobbies.json'))
-}
