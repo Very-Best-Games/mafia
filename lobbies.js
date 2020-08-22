664b03a2c46faed57f98c8b9f0e66ac04844928b
yankovskyandrey <YankovskyAndrey@gmail.com>
2020-08-22 18:30:45 +0300

    - rename room -> lobby - rename user -> player - add start-server command to run server - add morgan for logging all requests - add esm for using import/export on server side - use nanoid to generate unique room id - use correct fs calls to persist data - rename api handlers - use js code style - add lobbies.json with test data - move todo to separate file

---
+++ lobbies.js
@@ -0,0 +1,44 @@
+import { nanoid } from 'nanoid'
+import { save, load } from './persistence'
+
+export class Lobby {
+  constructor({ id = nanoid(), players = [] }) {
+    this.id = id
+    this.players = players
+  }
+
+  addPlayer(player) {
+    this.players.push(player)
+
+    save(lobbies)
+  }
+}
+
+export const lobbies = load().map(lobbyData => new Lobby(lobbyData))
+
+export const getAllLobbies = () => {
+  return lobbies
+}
+
+export const getLobbyById = (id) => {
+  return lobbies.find(lobby => lobby.id === id)
+}
+
+export const addLobby = () => {
+  let lobby = new Lobby()
+  lobbies.push(lobby)
+
+  save(lobbies)
+
+  return lobby
+}
+
+export function joinLobby(id, player) {
+  const lobby = getLobbyById(id)
+  if (lobby) {
+    lobby.addPlayer(player)
+    return lobby
+  }
+  return null
+}
+
