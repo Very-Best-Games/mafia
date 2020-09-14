09a762ba186efd0b5448e366f7013c8ddcfe2303
yankovskyandrey <YankovskyAndrey@gmail.com>
2020-09-14 19:17:54 +0300

    add nextjs

--- server/server.js
+++ server/server.js
@@ -4,86 +4,94 @@ import session from 'express-session'
 import path from 'path'
 // https://github.com/expressjs/morgan/issues/190
 import morgan from 'morgan'
+import next from 'next'
 
 import { getAllLobbies, getLobbyById, addLobby } from './lobbies'
 import { getAllPlayers, getPlayerById, addPlayer } from './players'
 
-const PORT = 3000
-const app = express()
-var FileStore = require('session-file-store')(session);
-var fileStoreOptions = {};
+const port = parseInt(process.env.PORT, 10) || 3000
 
-app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, 'public'))
-app.use(express.static(path.join(__dirname, 'public')))
+const dev = process.env.NODE_ENV !== 'production'
+const nextApp = next({ dev })
+const handle = nextApp.getRequestHandler()
 
-app.use(morgan('tiny'))
-app.set('trust proxy', 1)
+nextApp.prepare().then(() => {
+  const server = express()
 
-app.use(cookieParser());
-console.log('cookie parser')
+  var FileStore = require('session-file-store')(session);
+  var fileStoreOptions = {};
 
+  server.use(morgan('tiny'))
+  server.set('trust proxy', 1)
 
-app.use(session({
-  secret: 'TheBestGameEver',
-  store: new FileStore(fileStoreOptions),
-  resave: false,
-  saveUninitialized: true,
-}))
-console.log('session set')
+  server.use(cookieParser());
+  console.log('cookie parser')
 
-app.get('/', (req, res) => {
-  console.log('in main', req.session.id)
-  const player = getPlayerById(req.session.id)
-  console.log(player)
+  server.use(session({
+    secret: 'TheBestGameEver',
+    store: new FileStore(fileStoreOptions),
+    resave: false,
+    saveUninitialized: true,
+  }))
+  console.log('session set')
 
-  if (player) {
-    console.log('users session found.', req.session.id)
-    console.log('All users:', getAllPlayers())
-  } else {
-    console.log('users session not found.')
-    addPlayer(req.session.id)
-    console.log('All users:', getAllPlayers())
-  }
+  server.all((req, res, nextHandler) => {
+    console.log('in main', req.session.id)
+    const player = getPlayerById(req.session.id)
+    console.log(player)
 
-  res.render('./index.pug', { lobbies: getAllLobbies() })
-})
-
-app.get('/lobbies', (req, res) => {
-  res.json(getAllLobbies())
-})
-
-app.get('/lobbies/:lobbyId', (req, res) => {
-  const id = req.params.lobbyId
-  const lobby = getLobbyById(id)
-  if (lobby) {
-    res.json(lobby)
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.post('/lobbies', (req, res) => {
-  res.json(addLobby({players: [req.session.id]}))
-})
-
-app.post('/lobbies/:lobbyId', (req, res) => {
-  const lobbyId = req.params.lobbyId
-
-  const lobby = getLobbyById(lobbyId)
-  if (lobby) {
-    const playerId = req.session.id
-    if (!lobby.players.includes(playerId)) {
-      lobby.addPlayer(playerId)
-      return res.json(lobby)
+    if (player) {
+      console.log('users session found.', req.session.id)
+      console.log('All users:', getAllPlayers())
+    } else {
+      console.log('users session not found.')
+      addPlayer(req.session.id)
+      console.log('All users:', getAllPlayers())
     }
-    return res.sendStatus(400)
-  }
-  return res.sendStatus(404)
-})
 
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${ PORT }`)
-})
+    nextHandler()
+  })
 
+  server.get('/lobbies', (req, res) => {
+    res.json(getAllLobbies())
+  })
+
+  server.get('/lobbies/:lobbyId', (req, res) => {
+    const id = req.params.lobbyId
+    const lobby = getLobbyById(id)
+    if (lobby) {
+      res.json(lobby)
+    } else {
+      res.sendStatus(404)
+    }
+  })
+
+  server.post('/lobbies', (req, res) => {
+    res.json(addLobby({players: [req.session.id]}))
+  })
+
+  server.post('/lobbies/:lobbyId', (req, res) => {
+    const lobbyId = req.params.lobbyId
+
+    const lobby = getLobbyById(lobbyId)
+    if (lobby) {
+      const playerId = req.session.id
+      if (!lobby.players.includes(playerId)) {
+        lobby.addPlayer(playerId)
+        return res.json(lobby)
+      }
+      return res.sendStatus(400)
+    }
+    return res.sendStatus(404)
+  })
+
+  server.all('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
 
