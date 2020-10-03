deb1137b34afd50393d1aab76cbefb5193066dc3
Svoboda <sashasbox.forjob@gmail.com>
2020-10-03 19:32:38 +0200

    init menu page

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import './styles/global_styles.sass'
+import './styles/components/card.module.sass'
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import Head from "next/head";
+import Head from "next/head"
+import { useRouter } from 'next/router';
 
 export async function getServerSideProps() {
   const response = await fetch(`http://localhost:3000/api/lobbies`);
@@ -14,60 +15,46 @@ export default function Home({ lobbies }) {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Very Best Mafia</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <p>Welcome to The Mafia World!</p>
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Choose your Mafia city or create a new one.
         </p>
 
-        <ul>
-          {lobbiesSaved.map((lobby) => (
-            <li key={lobby.id}>
-              <span>{lobby.id}</span>
-              <ul>
-                {lobby.players.map((playerId) => (
-                  <li key={playerId}>{playerId}</li>
-                ))}
-              </ul>
-              <form
-                onSubmit={async (e) => {
-                  e.preventDefault();
-                  const response = await fetch(
-                    `http://localhost:3000/api/lobbies/${lobby.id}`,
-                    {
-                      method: "POST",
-                    }
-                  );
-                  const lobbyFromServer = await response.json();
-                  const index = lobbiesSaved.findIndex(
-                    (l) => l.id === lobbyFromServer.id
-                  );
-                  setLobbies(
-                    lobbiesSaved.map((item, i) => {
-                      if (i === index) {
-                        return lobbyFromServer;
-                      }
 
-                      return item;
-                    })
-                  );
-                }}
-              >
-                <button type="submit">Join</button>
-              </form>
-            </li>
-          ))}
-        </ul>
 
-        <button
-          type="button"
+        <div className="grid">
+          <a className="card"
+          onClick={async () => {
+            const response = await fetch("http://localhost:3000/api/lobbies", {
+              method: "POST",
+            });
+            const lobby = await response.json();
+            setLobbies(lobbiesSaved.concat(lobby));
+            const router = useRouter();
+            router.push('/'+lobby.id);
+
+          }}
+          >
+              <h3>Create a city &rarr;</h3>
+              <p>You can create your own city and establish your own rules in it.</p>
+          </a>
+
+          <a className="card"
+            href='/public_cities'
+          >
+              <h3>Join public city &rarr;</h3>
+              <p>You are welcome to any public city in the Mafia world.</p>
+          </a>
+
+          <a className="card"
           onClick={async () => {
             const response = await fetch("http://localhost:3000/api/lobbies", {
               method: "POST",
@@ -75,192 +62,24 @@ export default function Home({ lobbies }) {
             const lobby = await response.json();
             setLobbies(lobbiesSaved.concat(lobby));
           }}
-        >
-          Host
-        </button>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+              <h3>Join private city &rarr;</h3>
+              <p>If you have an invitation ticket to a city, you can join a private city.</p>
           </a>
-
-          <a
-            href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with ZEIT Now.
-            </p>
-          </a>
+          
         </div>
       </main>
 
       <footer>
         <a
-          href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
+          Powered by VeryBestGames
         </a>
       </footer>
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 }
