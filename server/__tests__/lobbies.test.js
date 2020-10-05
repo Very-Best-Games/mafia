3eeb052173b1024e108ad291bc624fc3e083d386
Lucas Turci <lucas.turci@gmail.com>
2020-10-05 17:25:48 -0300

    Update lobby tests to check on lobbies content instead of splice() calls

--- server/__tests__/lobbies.test.js
+++ server/__tests__/lobbies.test.js
@@ -1,4 +1,4 @@
-import { addLobby, LOBBY_LIFESPAN } from "../lobbies";
+import { addLobby, getAllLobbies, LOBBY_LIFESPAN } from "../lobbies";
 
 // Mock persistence so DB doesn't change during tests
 jest.mock("../persistence", () => ({
@@ -20,9 +20,8 @@ describe("Lobby actions", () => {
       };
       addLobby(lobbyData);
 
-      const spliceFn = jest.spyOn(Array.prototype, "splice");
       jest.advanceTimersByTime(LOBBY_LIFESPAN);
-      expect(spliceFn).toHaveBeenCalled();
+      expect(getAllLobbies()).toHaveLength(0);
     });
 
     it("shouldn't be deleted after 15min if players list changes in the middle", () => {
@@ -32,17 +31,16 @@ describe("Lobby actions", () => {
         code: "7025",
       };
       const lobby = addLobby(lobbyData);
-      const spliceFn = jest.spyOn(Array.prototype, "splice");
 
       jest.advanceTimersByTime(LOBBY_LIFESPAN / 2);
-      lobby.addPlayer("2VDBVQ-6MD2F1rWJXdgyBodBp0YjAY89"); // change player list in the middle
+      lobby.addPlayer("2VDBVQ-6MD2F1rWJXdgyBodBp0YjAY89"); // change player list in the mean time
       jest.advanceTimersByTime(LOBBY_LIFESPAN / 2);
 
-      expect(spliceFn).not.toHaveBeenCalled(); // did not call 15min after creation event
+      expect(getAllLobbies()).toContain(lobby); // did not delete 15min after creation event
 
       jest.advanceTimersByTime(LOBBY_LIFESPAN / 2);
 
-      expect(spliceFn).toHaveBeenCalled(); // did call 15min after player addition
+      expect(getAllLobbies()).toHaveLength(0); // did delete 15min after player addition
     });
 
     afterEach(() => {
