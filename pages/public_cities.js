8070ef64d573bc8315b076e58d8398a94055c9de
ALegostaeva <sashasbox.forjob@gmail.com>
2020-10-25 21:50:30 +0100

    join button

--- pages/public_cities.js
+++ pages/public_cities.js
@@ -22,49 +22,52 @@ export default function Home({ lobbies }) {
 
         <p className="description">Choose your destination.</p>
 
-        <ul>
-          {lobbiesSaved.map((lobby) => (
-            <li key={lobby.id}>
-              <span>
-                <Link href="/[lobbyId]" as={`/${lobby.id}`}>
-                  <a>{lobby.id}</a>
-                </Link>
-              </span>
-              <ul>
-                {lobby.players.map((playerId) => (
-                  <li key={playerId}>{playerId}</li>
-                ))}
-              </ul>
-              <form
-                onSubmit={async (e) => {
-                  e.preventDefault();
-                  const response = await fetch(
-                    `${process.env.NEXT_PUBLIC_HOST}/api/lobbies/${lobby.id}`,
-                    {
-                      method: "POST",
-                    }
-                  );
-                  console.log("here 2");
-                  const lobbyFromServer = await response.json();
-                  const index = lobbiesSaved.findIndex(
-                    (l) => l.id === lobbyFromServer.id
-                  );
-                  setLobbies(
-                    lobbiesSaved.map((item, i) => {
-                      if (i === index) {
-                        return lobbyFromServer;
+        <div className="public_city">
+          <ul className="city">
+            {lobbiesSaved.map((lobby) => (
+              <li key={lobby.id}>
+                <span>
+                  <Link href="/lobbies/[lobbyId]" as={`/lobbies/${lobby.id}`}>
+                    <a>{lobby.id}</a>
+                  </Link>
+                </span>
+                <ul>
+                  {lobby.players.map((playerId) => (
+                    <li key={playerId}>{playerId}</li>
+                  ))}
+                </ul>
+                <form
+                  onSubmit={async (e) => {
+                    e.preventDefault();
+                    const response = await fetch(
+                      `${process.env.NEXT_PUBLIC_HOST}/api/lobbies/${lobby.id}`,
+                      {
+                        method: "POST",
                       }
+                    );
+                    const lobbyFromServer = await response.json();
+                    const index = lobbiesSaved.findIndex(
+                      (l) => l.id === lobbyFromServer.id
+                    );
+                    setLobbies(
+                      lobbiesSaved.map((item, i) => {
+                        if (i === index) {
+                          return lobbyFromServer;
+                        }
 
-                      return item;
-                    })
-                  );
-                }}
-              >
-                <button type="submit">Join</button>
-              </form>
-            </li>
-          ))}
-        </ul>
+                        return item;
+                      })
+                    );
+                  }}
+                >
+                  <Link href="/lobbies/[lobbyId]" as={`/lobbies/${lobby.id}`}>
+                    <button type="submit">Join</button>
+                  </Link>
+                </form>
+              </li>
+            ))}
+          </ul>
+        </div>
 
         <button type="button" onClick={() => router.back()}>
           Click here to go back
